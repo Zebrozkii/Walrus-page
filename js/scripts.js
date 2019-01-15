66cb658a45c6ed1e362366a8ddf0152a9104e6a3
Jimmy Zebroski and Kenny Halim <jimmy@>
2019-01-15 10:29:04 -0800

    Added two buttons to switch background color

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,26 @@ $(document).ready(function() {
   });
 });
 $(document).ready(function(){
-  $(".clickable").click(function(){
+  $(".clickable1").click(function(){
     $(".baby-walrus-hidden").fadeToggle();
     $(".baby-walrus-showing").fadeToggle();
   });
 });
 $(document).ready(function(){
-  $(".clickable").click(function(){
+  $(".clickable2").click(function(){
     $(".walrus-suit-hidden").fadeToggle();
     $(".walrus-suit-showing").fadeToggle();
+  });
+});
+
+$(document).ready(function(){
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-background");
   });
 });
